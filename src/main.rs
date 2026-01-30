dfec0d0582d35d59ab90a8409da544cd2359e0d0
Eric Lee <nokeeo@me.com>
2026-01-29 23:02:08 -0800

    Uses clap to parse command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use clap::Parser;
 use ffgo::Config;
 use ffgo::jobs;
-use std::env;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    path: String
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> { 
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
     let mut dir_path = PathBuf::new();
-    dir_path.push(&args[1]);
-    let config = fs::read_to_string(format!("{}/job.toml", &args[1]))?;
+    dir_path.push(&args.path);
+    let config = fs::read_to_string(format!("{}/job.toml", &args.path))?;
     let config: Config = toml::from_str(&config)?;
     let queue = jobs::Queue::new();
     queue.push_directory(dir_path.as_path(), &config).await;
