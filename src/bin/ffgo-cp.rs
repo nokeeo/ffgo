02e6772869c04dc5a6a8dbe5f1e78df904870cb6
Eric Lee <nokeeo@me.com>
2026-02-03 00:50:35 -0800

    Adds an option to create a path before a remote copy when using ffgo-cp.

--- src/bin/ffgo-cp.rs
+++ src/bin/ffgo-cp.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use ffgo::file_constants::{JOB_CONFIG_FILE_NAME, READY_FILE_NAME};
+use regex::Regex;
 use std::env;
 use std::error::Error;
 use std::fs::File;
@@ -10,15 +11,68 @@ use std::process::Command;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+  #[arg(short)]
+  path_make: bool,
+
   #[arg(required = true, num_args = 1..)]
   source: Vec<PathBuf>,
+
   target: String,
+}
+
+#[derive(Debug)]
+struct RemoteUrl {
+  user: String,
+  host: String,
+  path: Option<PathBuf>,
+}
+
+impl RemoteUrl {
+  fn parse(str: &str) -> Result<RemoteUrl, String> {
+    let regex = Regex::new("^(?P<user>[a-z_][a-z0-9_-]*)@(?P<host>[a-zA-Z0-9.-]+)(?::(?P<path>.*))?$").unwrap();
+    let Some(captures) = regex.captures(str) else { 
+      return Err("Failed to parse remove URL".to_string());
+    };
+
+    let path = captures.name("path").map(|m| m.as_str());
+    let path = match path {
+      Some(path_str) => {
+        let mut path_buf = PathBuf::new();
+        path_buf.push(path_str);
+        Some(path_buf)
+      },
+      _ => None
+    };
+
+    Ok(RemoteUrl {
+      user: captures["user"].to_owned(),
+      host: captures["host"].to_owned(),
+      path: path,
+    })
+  }
 }
 
 fn files_contain_job_config(files: &Vec<PathBuf>) -> bool {
   files.iter()
     .filter(|p| p.file_name() == Some(*JOB_CONFIG_FILE_NAME))
     .count() > 0
+}
+
+fn make_dir_if_needed(target: &str) {
+  match RemoteUrl::parse(target) {
+    Ok(remote_url) => {
+      let Some(path) = remote_url.path else { return; };
+      println!("Creating directory {:?}", path);
+      let mut command = Command::new("ssh");
+      command.arg(format!("{}@{}", remote_url.user, remote_url.host));
+      command.arg("mkdir -p");
+      command.arg(path);
+
+      let mut handle = command.spawn().unwrap();
+      handle.wait();
+    },
+    _ => {},
+  }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -32,6 +86,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     if buffer.trim() != "y" {
       return Ok(());
     }
+  }
+
+  if args.path_make {
+    make_dir_if_needed(&args.target);
   }
 
   let mut temp_dir = PathBuf::new();
